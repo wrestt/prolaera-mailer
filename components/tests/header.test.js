accdbc0ad68095c469bdb85df646b9295f2d8f2a
Chris Hansen <chansen@Chriss-MacBook-Pro-2.local>
2018-06-04 10:48:36 -0700

    add one more todo

--- components/tests/header.test.js
+++ components/tests/header.test.js
@@ -1,3 +1,5 @@
 // TODO: Build out testing for different logos that properly handles logos with different sizes
 
 // TODO: Build out simple testing for differnt urls and text in the header
+
+// TODO: Build out testing for applying style to the header for mobile device. Centering logo and url herf.
