c30adbb5c84984cd82b6865b4d92c88f62e25c66
Chris Hansen <chansen@Chriss-MacBook-Pro-2.local>
2018-06-04 15:11:18 -0700

    added render

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { Email, renderEmail } from 'react-html-email';
+import { renderEmail } from 'react-html-email';
 import userComplaince from './templates/userCompliance';
 
 const handler = (event, context) => {
   const { body } = event;
   switch (body.template) {
     case 'userComplaince':
-      const email = userComplaince(body.completeCompliance, body.logoUrl);
+      const email = renderEmail(userComplaince(body.completeCompliance, body.logoUrl));
       return context.succed(email);
     default:
       context.fail('Invaild Template');
