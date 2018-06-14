cf1f43e4df7abff91db32ff410316104a6b5a310
dtuck43 <dtuck43@gmail.com>
2018-06-14 10:55:02 -0700

    Add testing for domHelpers, specifically setInnerHtml function

--- templates/tests/registration.test.js
+++ templates/tests/registration.test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import EventInfo from '../../components/registration/eventInfo';
+import setInnerHtml from '../../lib/domHelpers';
 import completeRegistration from '../../templates/tests/completeRegistration';
 import registrationEmail from '../registrationBuilder';
 describe('registration Email', () => {
@@ -35,6 +36,20 @@ describe('registration Email', () => {
     const email = await registrationEmail(completeRegistration, logoUrl);
     const emailHtml = await writeFile(email);
     expect(emailHtml).toMatchSnapshot();
+  });
+});
+
+describe('domHelper functions', () => {
+  it('checks setInnerHtml function is returning parsed data correctly', async () => {
+    const testData1 = '<TABLE><tr><td>HELLO</tr></TABL>';
+    const parsedData1 = setInnerHtml(testData1);
+    const testData2 = '<svg><g/onload=alert(2)//<p>';
+    const parsedData2 = setInnerHtml(testData2);
+    const testData3 = '<UL><li><A HREF=//google.com>click</UL>';
+    const parsedData3 = setInnerHtml(testData3);
+    expect(parsedData1).toEqual({ __html: '<table><tbody><tr><td>HELLO</td></tr></tbody></table>' });
+    expect(parsedData2).toEqual({ __html: '<svg><g></g></svg>' });
+    expect(parsedData3).toEqual({ __html: '<ul><li><a href="//google.com">click</a></li></ul>' });
   });
 });
 
