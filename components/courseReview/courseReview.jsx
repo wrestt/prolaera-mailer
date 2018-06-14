a70257c7eca209bacc3065357177610e53e0d9cb
Dillon <dillontitcomb@gmail.com>
2018-06-14 15:38:43 -0700

    add styling to courseReview template including differing font weights and indented course information.

--- components/courseReview/courseReview.jsx
+++ components/courseReview/courseReview.jsx
@@ -30,20 +30,25 @@ class CourseReview extends React.Component {
           border: '1px solid lightgray',
           width: '500px',
           padding: '20px',
-          fontFamily: 'Arial, sans-serif'
+          fontFamily: 'Arial, sans-serif',
+          marginBottom: '20px'
         }}
       >
         <Box className={'courseReview'} align="center" width="500px">
           <Item align="left">
-            <p>You have been selected to review {courseName}.</p>
+            <p style={{ fontSize: '18px', fontWeight: '300', marginTop: '5px', marginBottom: '5px' }}>
+              You have been selected to review {courseName}.
+            </p>
           </Item>
           <Item align="left">
-            <div style={{ paddingLeft: '50px' }}>
-              <h3 style={{ display: 'inline' }}>By: </h3>
-              <p style={{ display: 'inline', fontSize: '18px' }}> {by}</p>
+            <div style={{ paddingLeft: '40px' }}>
+              <h1>Course Information:</h1>
+              <h3>
+                By: <span style={{ fontWeight: '300' }}>{by}</span>
+              </h3>
               <h3>Recommended CPE Credit(s):</h3>
               <ul>
-                <li>
+                <li style={{ fontSize: '18px', fontWeight: '300' }}>
                   {recommendedCredits} - {creditHours} Hour(s)
                 </li>
               </ul>
@@ -53,24 +58,34 @@ class CourseReview extends React.Component {
               <h3>
                 Price: <span style={{ fontWeight: '300' }}>{{ price } > 0 ? { price } : 'Free'}</span>
               </h3>
-              <h3 style={{ display: 'inline' }}>Target Audience: </h3>
-              <span style={{ display: 'inline', fontSize: '18px' }}>{targetAudience}</span>
+              <h3>
+                Target Audience: <span style={{ fontWeight: '300' }}>{targetAudience}</span>
+              </h3>
               <h3>Prep:</h3>
-              <div dangerouslySetInnerHTML={{ __html: prep }} />
+              <div style={{ fontSize: '18px', fontWeight: '300' }} dangerouslySetInnerHTML={{ __html: prep }} />
               <h3>Prerequisites:</h3>
-              <div dangerouslySetInnerHTML={{ __html: prerequisites }} />
+              <div
+                style={{ fontSize: '18px', fontWeight: '300' }}
+                dangerouslySetInnerHTML={{ __html: prerequisites }}
+              />
               <h3>
                 Level: <span style={{ fontWeight: '300' }}>{level}</span>
               </h3>
               <h3>
-                Learning Objectives: <div dangerouslySetInnerHTML={{ __html: learningObjectives }} />
+                Learning Objectives:{' '}
+                <div
+                  style={{ fontSize: '18px', fontWeight: '300' }}
+                  dangerouslySetInnerHTML={{ __html: learningObjectives }}
+                />
               </h3>
               <h3>Description:</h3>
-              <div dangerouslySetInnerHTML={{ __html: description }} />
+              <div style={{ fontSize: '18px', fontWeight: '300' }} dangerouslySetInnerHTML={{ __html: description }} />
             </div>
           </Item>
           <Item align="left">
-            <p>More information about this course can be found by navigating to the Prolaera website.</p>
+            <p style={{ fontSize: '18px', fontWeight: '300' }}>
+              More information about this course can be found by navigating to the Prolaera website.
+            </p>
             <div
               className="buttonContainer"
               style={{ width: '100%', textAlign: 'center', padding: '20px 0px 20px 0px' }}
