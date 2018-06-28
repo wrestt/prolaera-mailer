bd25cce3ab97a20a6b0bc45bf0ac771b5ce55e41
Dillon <dillontitcomb@gmail.com>
2018-06-27 19:07:27 -0700

    refactor userCompliance to add an extra row and hide data that is not applicable.

--- components/compliance/categoryRow.jsx
+++ components/compliance/categoryRow.jsx
@@ -6,29 +6,23 @@ class CategoryRow extends React.Component {
   render() {
     const { flatCompliance, regulator, reportIndex, category } = this.props;
     const hoursEarned = flatCompliance.applied[category];
+    const hoursApplied = flatCompliance.applied[category];
     const hoursRequired = regulator.cycle_limiter.min[category] ? regulator.cycle_limiter.min[category] : 'N/A';
     const hoursRemaining = hoursRequired !== 'N/A' && hoursRequired - hoursEarned > 0 ? hoursRequired - hoursEarned : 0;
     return (
       <div>
         <tr>
-          <td
-            style={{
-              fontSize: '16px',
-              fontFamily: 'verdana,geneva,sans-serif',
-              verticalAlign: 'top'
-            }}
-          >
+          <td style={{ fontSize: '16px', fontFamily: 'verdana,geneva,sans-serif', verticalAlign: 'top' }}>
             <span>{categoryName(category)}</span>
           </td>
           <ComplianceBox text="Hours Earned" hours={hoursEarned} color={reportIndex} />
-          <ComplianceBox text="Hours Required" hours={hoursRequired} color={reportIndex} />
+          <ComplianceBox text="Hours Applied" hours={hoursApplied} color={reportIndex} />
+          {hoursRequired == 'N/A' ? (
+            <ComplianceBox text=" " hours=" " color="white" />
+          ) : (
+            <ComplianceBox text="Hours Required" hours={hoursRequired} color={reportIndex} />
+          )}
           <ComplianceBox text="Remaining" hours={hoursRemaining} color={reportIndex} />
-        </tr>
-        <tr>
-          <td colSpan="4" style={{ borderBottom: '1px #333333 solid', paddingTop: '4px' }} />
-        </tr>
-        <tr>
-          <td colSpan="4" style={{ paddingBottom: '3px' }} />
         </tr>
       </div>
     );
--- components/compliance/yearRow.jsx
+++ components/compliance/yearRow.jsx
@@ -4,53 +4,21 @@ import ComplianceBox from './complianceBox';
 class YearRow extends React.Component {
   render() {
     const { compliance, regulator, reportIndex } = this.props;
-    if (
-      !regulator.cycle_limiter.annual ||
-      !regulator.cycle_limiter.annual.hours
-    )
-      return null;
+    if (!regulator.cycle_limiter.annual || !regulator.cycle_limiter.annual.hours) return null;
     const hoursEarned = compliance.total;
+    const hoursApplied = compliance.total;
     const hoursRequired = regulator.cycle_limiter.annual.hours;
-    const hoursRemaining =
-      hoursRequired !== 'N/A' && hoursRequired - hoursEarned > 0
-        ? hoursRequired - hoursEarned
-        : 0;
+    const hoursRemaining = hoursRequired !== 'N/A' && hoursRequired - hoursEarned > 0 ? hoursRequired - hoursEarned : 0;
     return (
       <div>
         <tr>
-          <td
-            style={{
-              fontSize: '16px',
-              fontFamily: 'verdana,geneva,sans-serif',
-              verticalAlign: 'top'
-            }}
-          >
+          <td style={{ fontSize: '16px', fontFamily: 'verdana,geneva,sans-serif', verticalAlign: 'top' }}>
             <span>Year</span>
           </td>
-          <ComplianceBox
-            text="Hours Earned"
-            hours={hoursEarned}
-            color={reportIndex}
-          />
-          <ComplianceBox
-            text="Hours Required"
-            hours={hoursRequired}
-            color={reportIndex}
-          />
-          <ComplianceBox
-            text="Remaining"
-            hours={hoursRemaining}
-            color={reportIndex}
-          />
-        </tr>
-        <tr>
-          <td
-            colSpan="4"
-            style={{ borderBottom: '1px #333333 solid', paddingTop: '4px' }}
-          />
-        </tr>
-        <tr>
-          <td colSpan="4" style={{ paddingBottom: '3px' }} />
+          <ComplianceBox text="Hours Earned" hours={hoursEarned} color={reportIndex} />
+          <ComplianceBox text="Hours Applied" hours={hoursApplied} color={reportIndex} />
+          <ComplianceBox text="Hours Required" hours={hoursRequired} color={reportIndex} />
+          <ComplianceBox text="Remaining" hours={hoursRemaining} color={reportIndex} />
         </tr>
       </div>
     );
